dd4c64459fa61dfca1ef6d53a9e2964afac6866b
VladislavDevs <supermars5327@gmail.com>
2024-10-05 08:23:18 +0700

    Исправление ошибки

--- PeoplesList.py
+++ PeoplesList.py
@@ -1,7 +1,7 @@
 boysString = input("Введите список мужчин через запятую: ")
 girlsString = input("Введите список женщин через запятую: ")
-boysList = boysString.split()
-girlsList = girlsString.split()
+boysList = boysString.split(",")
+girlsList = girlsString.split(",")
 
 if((len(boysString) == 0 or len(girlsString) == 0) or (len(boysList) != len(girlsList))):
     print("Знакомство не возможно! В списках есть люди которые останутся без пары.")
